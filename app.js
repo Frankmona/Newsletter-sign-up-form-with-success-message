a67854aa9a1c54a64291313ca183d43ad92451c6
Frankmona <frankoguche1@gmail.com>
2023-07-26 13:51:44 +0100

    updated js

---
+++ app.js
@@ -0,0 +1,49 @@
+const form = document.getElementById("form");
+const email = document.getElementById("email");
+const successMessage = document.querySelectorAll('.success-message')
+const DismissButton = document.getElementById('button')
+
+// function that shows the error
+function showError(input){
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error'
+    const p = formControl.querySelector('p')
+}
+
+// function that shows the success
+function showSuccess(){
+    successMessage[0].classList.add('success');
+    successMessage[1].classList.add('success');
+
+}
+
+// function that removes success class
+function removeSuccess(){
+    successMessage[0].classList.remove('success');
+    successMessage[1].classList.remove('success');
+}
+
+
+// Event listener listening for the submit event
+form.addEventListener('submit', function(e){
+        e.preventDefault()
+// console.log('frank');
+    if(email.value === ''){
+        showError(email);
+     }
+    else{
+        showSuccess();
+     }
+
+})
+
+// Event listener listening for the click dismissal event
+DismissButton.addEventListener('click', function(e){
+    e.preventDefault()
+    // console.log('frank');
+    if (successMessage[1].classList.contains("success")) {
+        removeSuccess();
+      }
+})
+
+    
